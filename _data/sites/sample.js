563890708545d42ee9422f2fa4d823283f68f60d
Simon Vreeman <simonvreeman@gmail.com>
2020-09-12 17:58:31 +0200

    Update sample.js

--- _data/sites/sample.js
+++ _data/sites/sample.js
@@ -2,7 +2,8 @@ module.exports = {
 	name: "Sample", // optional, falls back to object key
 	description: "The default sites that get tested",
 	options: {
-		frequency: 60 * 23, // (in minutes), 23 hours
+		// frequency: 60 * 23, // (in minutes), 23 hours
+    frequency: 15, // (in minutes), 23 hours
 	},
 	urls: [
 		"https://www.speedlify.dev/",
