9792cd35d43f6f4092bbdd13bb9b655401f060d5
Simon Vreeman <simonvreeman@gmail.com>
2020-09-14 13:33:20 +0200

    Update sites.js

--- _data/sites/sites.js
+++ _data/sites/sites.js
@@ -1,14 +1,13 @@
 module.exports = {
-	name: "Sites", // optional, falls back to object key
-	description: "The default sites that get tested",
-	options: {
-		// frequency: 60 * 23, // (in minutes), 23 hours
-    frequency: 60 * 1, // (in minutes), 23 hours
+  name: "Sites", // optional, falls back to object key
+  description: "The default sites that get tested",
+  options: {
+    // frequency: 60 * 23, // (in minutes), 23 hours
+    frequency: 60 * 6, // (in minutes), 6 hours
     freshChrome: "run",
-	},
-	urls: [
-		"https://www.speedlify.dev/",
+  },
+  urls: [
     "https://vreeman.com/",
     "https://www.vanmoof.com/en-NL",
-	]
+  ]
 };
